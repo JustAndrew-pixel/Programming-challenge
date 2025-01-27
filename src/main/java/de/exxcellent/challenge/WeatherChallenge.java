79b123007ea74871e8d40acf7247d4e08791a75c
Andrej <anweit00@hs-esslingen.de>
2025-01-27 18:44:33 +0100

    feat: add method for calculating temperature spread

---
+++ src/main/java/de/exxcellent/challenge/WeatherChallenge.java
@@ -0,0 +1,33 @@
+package de.exxcellent.challenge;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
+import java.util.OptionalDouble;
+
+public class WeatherChallenge {
+
+    public static double[] calculateTemperatureSpread(List<String[]> temperatureArrays) {
+        if (temperatureArrays == null) {
+            throw new IllegalArgumentException("Input list cannot be null");
+        }
+
+        double[] result = new double[temperatureArrays.size()];
+
+        for (int i = 0; i < temperatureArrays.size(); i++) {
+            String[] temperatureRow = temperatureArrays.get(i);
+
+            double maxTemperature = Double.parseDouble(temperatureRow[1]);
+            double minTemperature = Double.parseDouble(temperatureRow[2]);
+            result[i] = maxTemperature - minTemperature;
+        }
+
+        return result;
+    }
+}
+
+
